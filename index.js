12186b16f3a618c48cb1a7996abcdec5dc836131
Hwm333 <h.w.m3@outlook.com>
2026-04-14 16:59:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ app.post("/whatsapp", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () =>
+app.listen(process.env.PORT || 3000, '0.0.0.0', () =>
   console.log(`🚀 Server running on port ${process.env.PORT || 3000}`)
 );
